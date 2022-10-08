f214e0376eab626fb239f2d4c87ae704704e43b5
As-If13101245 <93853766+As-If13101245@users.noreply.github.com>
2022-10-08 13:35:01 +0600

    v5

--- store/views.py
+++ store/views.py
@@ -60,5 +60,6 @@ def search(request):
     context = {
         'products': products,
         'product_count' : product_count,
+        'keyword' : keyword,
     }
     return render(request, 'store/store.html', context)
