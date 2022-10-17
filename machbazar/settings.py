db115a374504a3413908c3fb5228f6644f084562
As-If13101245 <asifasraf13101245@gmail.com>
2022-10-17 23:15:33 +0600

    Mac

--- machbazar/settings.py
+++ machbazar/settings.py
@@ -139,5 +139,12 @@ MEDIA_ROOT = BASE_DIR /'media'
 from django.contrib.messages import constants as messages
 MESSAGE_TAGS = {
     messages.ERROR: 'danger',
-    
+
 }
+
+# SMTP configuration
+EMAIL_HOST = 'smtp.gmail.com'
+EMAIL_PORT = 587
+EMAIL_HOST_USER = 'asifasraf46@gmail.com'
+EMAIL_HOST_PASSWORD = 'auljchxcnrnfzbax'
+EMAIL_USE_TLS = True
